30a244dc34e7dace5ff045e5936e0fb920f282c0
kxy <3293957379@qq.com>
2025-03-11 22:13:51 +0800

    kxy-3.11 settings完善

--- assets/Script/StartController.ts
+++ assets/Script/StartController.ts
@@ -1,12 +1,31 @@
-import { _decorator, Component, Node, Animation } from 'cc';
+import { _decorator, Component, Node, Animation,Label, Vec3,EventMouse,input,Input, EventTouch,UITransform} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('StartController')
 export class StartController extends Component {
     @property(Animation)
     BodyAnim: Animation = null;
+    @property(Animation)
+    BodyAnim1: Animation = null;
+    @property(Node)
+    tipsPanel:Node=null;
+    @property(Node)
+    Settings:Node=null;
+    onLoad()
+    {
+        this.tipsPanel.active = false;
+        this.Settings.on(Node.EventType.MOUSE_ENTER, this.onMouseEnter, this);
+        this.Settings.on(Node.EventType.MOUSE_LEAVE, this.onMouseLeave, this);
+    }
+    private onMouseEnter() {
+        this.tipsPanel.active = true;
+    }
+    private onMouseLeave() {
+        this.tipsPanel.active = false;
+    }
     start() {
         this.BodyAnim.play('oneStep');
+        this.BodyAnim1.play('tStep');
     }
 
     update(deltaTime: number) {
