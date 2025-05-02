38666286e335906044f7f23e271ea332c5450d23
linshule <linshule@foxmail.com>
2025-05-02 17:27:11 +0800

    lsl-完成第二关

--- assets/Script/PlayerController.ts
+++ assets/Script/PlayerController.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, EventTarget, Input, input, EventKeyboard, KeyCode, RigidBody, Vec3, RigidBody2D, Vec2, Animation, Collider2D, Contact2DType, PhysicsSystem2D, IPhysics2DContact } from 'cc';
+import { _decorator, Component, Node, EventTarget, Input, input, EventKeyboard, KeyCode, RigidBody, Vec3, RigidBody2D, Vec2, Animation, Collider2D, Contact2DType, PhysicsSystem2D, IPhysics2DContact, director } from 'cc';
 import { curLevel, LevelManager } from './LevelManager';
 const { ccclass, property } = _decorator;
 
@@ -16,6 +16,12 @@ export class PlayerController extends Component {
     private jumpForce: number = 10;
     private jumpProgress: number = 0;
     private isBlack: boolean = false;
+    private isCitie: boolean = false;
+    private fireCount: number = 3;
+    private forceMagnitude: number = 1; // 作用力大小
+
+    @property({ type: Number })
+    private Level: Number = 0;
 
     @property({ type: Node })
     private backGround: Node | null = null;
@@ -24,6 +30,8 @@ export class PlayerController extends Component {
 
     start() {
         this.levelCtrl = this.backGround.getComponent(LevelManager);
+        curLevel.setValue(Number(this.Level));
+        console.log(curLevel.value);
         this.init();
     }
 
@@ -38,8 +46,19 @@ export class PlayerController extends Component {
             collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
             collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
         }
+        if (curLevel.value == 1) {
+            this.node.setPosition(new Vec3(-8320, -800, 0));
+            this.maxJumpCount = 1;
+            this.jumpProgress = 0;
+            this.isBlack = false;
+        } else if (curLevel.value == 2) {
+            this.node.setPosition(new Vec3(-8320, -800, 0));
+            this.jumpProgress = 3;
+            this.maxJumpCount = 2;
+            this.isBlack = true;
+            this.fireCount = 3;
+        }
 
-        this.node.setPosition(new Vec3(-8320, -800, 0));
     }
 
     onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact) {
@@ -79,16 +98,51 @@ export class PlayerController extends Component {
             if (this.jumpProgress == 3) {
                 this.maxJumpCount = 2;
             }
+            if (this.jumpProgress == 4) {
+                input.on(Input.EventType.KEY_DOWN, this.onCitieDown, this);
+                input.on(Input.EventType.KEY_UP, this.onCitieUp, this);
+            }
+            if (this.jumpProgress == 5) {
+                this.fireCount = 0;
+            }
             selfCollider.body.enabledContactListener = true;
         }
         if (otherCollider.group === PhysicsSystem2D.PhysicsGroup.DOOR) {
             if (this.jumpProgress == 3) {
                 curLevel.setValue(curLevel.value + 1);
+                console.log(curLevel.value);
+                this.levelCtrl.toNewLevel();
+            }
+            if (this.jumpProgress == 5) {
+                curLevel.setValue(curLevel.value + 1);
                 this.levelCtrl.toNewLevel();
             }
         }
+        if (otherCollider.group === PhysicsSystem2D.PhysicsGroup.FIRE) {
+            if (this.fireCount <= 1) {
+                selfCollider.body.enabledContactListener = false;
+                otherCollider.node.getComponent(Animation).play('pzxs');
+                otherCollider.sensor = true;
+                otherCollider.group = PhysicsSystem2D.PhysicsGroup.NOFIRE;
+                this.fireCount++;
+                selfCollider.body.enabledContactListener = true;
+            }
+        }
     }
-
+    onCitieDown(event: EventKeyboard) {
+        switch (event.keyCode) {
+            case KeyCode.KEY_J:
+                this.isCitie = true;
+                break;
+        }
+    }
+    onCitieUp(event: EventKeyboard) {
+        switch (event.keyCode) {
+            case KeyCode.KEY_J:
+                this.isCitie = false;
+                break;
+        }
+    }
     onEndContact() {
         this.isGrounded = false;
     }
@@ -151,6 +205,40 @@ export class PlayerController extends Component {
         }
         if (this.node.getPosition().x < -8630 || this.node.getPosition().y < -4710 || this.node.getPosition().x > 7400) {
             this.node.setPosition(new Vec3(-8320, -800, 0));
+        }
+        if (this.isCitie) {
+            const manualBlocks = [
+                { x: 308, y: 445 },
+                { x: 757, y: 536 },
+                { x: 1085, y: 337 },
+                { x: 835, y: 158 },
+                { x: 1250, y: 536 }
+            ];
+            let tax = -332;
+            let tay = 85;
+            let curx = this.node.worldPosition.x;
+            let cury = this.node.worldPosition.y;
+            let md = 1000000000;
+            for (const blockInfo of manualBlocks) {
+                let d = Math.sqrt((curx - blockInfo.x) * (curx - blockInfo.x) + (cury - blockInfo.y) * (cury - blockInfo.y));
+                if (d < md) {
+                    md = d;
+                    tax = blockInfo.x;
+                    tay = blockInfo.y;
+
+                }
+            }
+            const targetPos = new Vec2(tax, tay);
+            const currentPos = new Vec2(curx, cury);
+            const direction = new Vec2();
+            //       console.log(currentPos);
+            //        console.log(targetPos);
+            Vec2.subtract(direction, targetPos, currentPos);
+            const force = direction.multiplyScalar(this.forceMagnitude)
+            this.rigidPlayer.applyForceToCenter(force, true);
+            this.node.getChildByName('citie').active = true;
+        } else {
+            this.node.getChildByName('citie').active = false;
         }
     }
 }
--- assets/Script/TwoLevelManager.ts
+++ assets/Script/TwoLevelManager.ts
@@ -1,11 +1,8 @@
 import { _decorator, CCInteger, Component, instantiate, Node, Prefab, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
-enum BlockType {
-    BT_NONE,
-    BT_STONE,
-};
-@ccclass('Level1Manage')
-export class Level1Manage extends Component {
+
+@ccclass('TwoLevelManager')
+export class TwoLevelManager extends Component {
     @property({ type: Prefab })
     public boxPrefab1: Prefab | null = null;
     @property({ type: Prefab })
@@ -13,13 +10,21 @@ export class Level1Manage extends Component {
     @property({ type: Prefab })
     public boxPrefab3: Prefab | null = null;
     @property({ type: Prefab })
+    public boxPrefab4: Prefab | null = null;
+    @property({ type: Prefab })
+    public boxPrefab5: Prefab | null = null;
+    @property({ type: Prefab })
     public doorstar: Prefab | null = null;
     @property(Prefab)
     public doorend: Prefab | null = null;
     @property(Prefab)
     public tool: Prefab | null = null;
     @property(Prefab)
-    public black: Prefab | null = null;
+    public fire: Prefab | null = null;
+    @property(Prefab)
+    public citie: Prefab | null = null;
+    @property(Prefab)
+    public jian: Prefab | null = null;
     @property(Node)
     public playerBase: Node | null = null;
 
@@ -40,35 +45,14 @@ export class Level1Manage extends Component {
         this.generateMap6();
         this.generateMap7();
         this.generateMap8();
-        this.generateBlack();
-    }
-    generateBlack() {
-        this._road = [];
-        for (let i = 1; i <= this.roadHeight; i++) {
-            let _roadCur: boolean[] = [];
-            for (let j = 1; j <= this.roadLength; j++) {
-                _roadCur[j] = true;
-            }
-            this._road[i] = _roadCur;
-        }
-        this._road[1][2] = false;
-        this._road[1][3] = false;
-        for (let i = 1; i <= this.roadHeight; i++) {
-            for (let j = 1; j <= this.roadLength; j++) {
-                let block: Node | null = instantiate(this.black);
-                if (!this._road[i][j]) block = null;
-                if (block) {
-                    block.setPosition(i * this.BLOCK_SIZE - 9700, j * this.BLOCK_SIZE - 6150, 0);
-                    this.playerBase.addChild(block);
-                }
-            }
-        }
     }
     generateMap1() {
         const manualBlocks = [
-            { x: -345, y: -188, prefab: this.boxPrefab1 },
-            { x: -145, y: -188, prefab: this.boxPrefab2 },
-            { x: -510, y: -105, prefab: this.boxPrefab3 }
+            { x: -535, y: 144, prefab: this.boxPrefab1 },
+            { x: -332, y: 144, prefab: this.boxPrefab1 },
+            { x: 212, y: -138, prefab: this.boxPrefab1 },
+            { x: 68, y: -242, prefab: this.boxPrefab1 },
+            { x: -82, y: 229, prefab: this.boxPrefab1 }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -78,12 +62,12 @@ export class Level1Manage extends Component {
                 block.parent = this.node;
             }
         }
-    }
-    generateMap2() {
+    } generateMap2() {
         const manualBlocks = [
-            { x: -245, y: -270, prefab: this.boxPrefab1 },
-            { x: -260, y: -10, prefab: this.boxPrefab3 },
-            { x: -260, y: 74, prefab: this.boxPrefab1 }
+            { x: 160, y: 230, prefab: this.boxPrefab2 },
+            { x: 585, y: 229, prefab: this.boxPrefab2 },
+            { x: 424, y: 36, prefab: this.boxPrefab2 },
+            { x: 596, y: -258, prefab: this.boxPrefab2 }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -96,9 +80,9 @@ export class Level1Manage extends Component {
     }
     generateMap3() {
         const manualBlocks = [
-            { x: -94, y: 150, prefab: this.boxPrefab3 },
-            { x: 112, y: 68, prefab: this.boxPrefab2 },
-            { x: 598, y: -200, prefab: this.boxPrefab1 }
+            { x: -108, y: 45, prefab: this.boxPrefab3 },
+            { x: -545, y: -63, prefab: this.boxPrefab3 },
+            { x: -409, y: -252, prefab: this.boxPrefab3 }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -111,9 +95,7 @@ export class Level1Manage extends Component {
     }
     generateMap4() {
         const manualBlocks = [
-            { x: 240, y: -24, prefab: this.boxPrefab3 },
-            { x: 200, y: -188, prefab: this.boxPrefab2 },
-            { x: 75, y: 235, prefab: this.boxPrefab1 }
+            { x: -204, y: -143, prefab: this.boxPrefab4 }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -126,9 +108,7 @@ export class Level1Manage extends Component {
     }
     generateMap5() {
         const manualBlocks = [
-            { x: 500, y: -110, prefab: this.boxPrefab2 },
-            { x: 40, y: -110, prefab: this.boxPrefab2 },
-            { x: 320, y: -110, prefab: this.boxPrefab1 }
+            { x: 629, y: -194, prefab: this.doorend }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -141,9 +121,9 @@ export class Level1Manage extends Component {
     }
     generateMap6() {
         const manualBlocks = [
-            { x: 370, y: 155, prefab: this.boxPrefab2 },
-            { x: 555, y: 250, prefab: this.boxPrefab2 },
-            { x: 430, y: -260, prefab: this.boxPrefab2 }
+            { x: -164, y: -65, prefab: this.fire },
+            { x: -101, y: 113, prefab: this.fire },
+            { x: 482, y: 113, prefab: this.fire }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -156,8 +136,11 @@ export class Level1Manage extends Component {
     }
     generateMap7() {
         const manualBlocks = [
-            //   { x: -650, y: -45, prefab: this.doorstar },
-            { x: 620, y: -140, prefab: this.doorend }
+            { x: -332, y: 85, prefab: this.jian },
+            { x: 117, y: 176, prefab: this.jian },
+            { x: 610, y: 176, prefab: this.jian },
+            { x: 445, y: -23, prefab: this.jian },
+            { x: 195, y: -202, prefab: this.jian }
         ];
 
         for (const blockInfo of manualBlocks) {
@@ -165,14 +148,14 @@ export class Level1Manage extends Component {
                 const block = instantiate(blockInfo.prefab);
                 block.setPosition(blockInfo.x, blockInfo.y, 0);
                 block.parent = this.node;
+                //      console.log(block.worldPosition);
             }
         }
     }
     generateMap8() {
         const manualBlocks = [
-            { x: -490, y: -58, prefab: this.tool },
-            { x: -330, y: 50, prefab: this.tool },
-            { x: 405, y: 210, prefab: this.tool }
+            { x: -432, y: -10, prefab: this.citie },
+            { x: -22, y: 84, prefab: this.tool }
 
         ];
 
@@ -187,5 +170,6 @@ export class Level1Manage extends Component {
     update(deltaTime: number) {
 
     }
-
 }
+
+
